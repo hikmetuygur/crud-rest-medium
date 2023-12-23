1e6d81866239c047f9d13af4d125cd60103b1741
hikmet uygur <uygurhikmet@gmail.com>
2023-12-24 01:16:00 +0300

    Initial commit.

---
+++ Database Scripts/employee_tracker.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE  IF NOT EXISTS `employee_tracker`;
+USE `employee_tracker`;
+CREATE TABLE `employee` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name`varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
---
+++ crud-rest-medium/src/main/java/com/uygur/crudrestmedium/CrudRestMediumApplication.java
@@ -0,0 +1,13 @@
+package com.uygur.crudrestmedium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRestMediumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudRestMediumApplication.class, args);
+	}
+
+}
